8ec8a7f6ea3a7e6dd0adad8a7b308eefd36f1afa
tran quang duy - 0495 <duytran1652004@gmail.com>
2026-06-09 14:17:38 +0700

    Remove .env from repository, add ca.pem, update db.js

--- BE/config/db.js
+++ BE/config/db.js
@@ -1,11 +1,18 @@
 const mysql = require('mysql2');
 require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: {
+    ca: fs.readFileSync(path.join(__dirname, 'ca.pem')),
+    rejectUnauthorized: true
+  },
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
